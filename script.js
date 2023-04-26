d90830a2d59df75e83da8221f9fee1596fc2877f
soniaj27 <118751361+soniaj27@users.noreply.github.com>
2023-04-26 14:03:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var c = document.getElementbyId('level').value;
+
+var c = document.getElementById("level");
+var level = 0;
+while (c.parentNode) {
+  level++;
+  c = c.parentNode;
+}
+alert("DOM level of #level:",level);
+var message = "The level of the element is: " + level;
+alert(message);
